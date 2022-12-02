2baeb9dbd9928000ae9c8ac2a32be4617901f679
Have Samuel <samiehave9@gmail.com>
2022-12-02 20:09:52 +0300

    Dynamic Property Access and SettingProperty Dynamically

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -3,12 +3,15 @@ const movieList = document.getElementById('movie-list');
 movieList.style['background-color'] = 'green';
 movieList.style.display = 'block';
 
-const person = {
+const anyVariableFromTheUser = 'level';
+
+let person = {
   'first name': 'Have',
   age: 30,
   hobbies: ['Gym', 'Swmming'],
+  [anyVariableFromTheUser]: '...', // can be used
   greet: function () {
-    
+    alert('Hey There!');
   },
   5.8: 'Hello',
 };
@@ -18,7 +21,9 @@ const person = {
 // later you will set age to some value
 delete person.age;
 person.isAdmin = true;
+// Dynamic Property Access
+const keyName = 'first name';
 
-console.log(person['first name']);
+console.log(person[keyName]);
 console.log(person[5.8]);
 console.log(person);
