2464359ff44784dbaa4290de1444622d5b296bd6
Have Samuel <samiehave9@gmail.com>
2022-12-02 19:25:11 +0300

    Add and overwrite anything in the object

---
+++ assets/scripts/objects.js
@@ -0,0 +1,13 @@
+const person = {
+  name: 'Have',
+  age: 30,
+  hobbies: ['Gym', 'Swmming'],
+  greet: function () {
+    
+  }
+};
+
+person.age = 35;
+person.isAdmin = true;
+
+console.log(person);
